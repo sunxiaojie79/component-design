b8f8b3266559569a66a7d2ca78a3f097f218f608
sunxiaojie79 <297280911@qq.com>
2020-06-13 22:07:47 +0800

    feat: 添加Button 测试代码

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Button from './button'
 
-test('first react test case', () => { 
-  const wraper = render(<Button>nice</Button>)
-  const element = wraper.queryByText('nice')
-  expect(element).toBeTruthy()
+describe('test Button component', () => { 
+  it('should render the correct default button', () => { 
+    const wraper = render(<Button>nice</Button>)
+    const element = wraper.getByText('nice')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+  })
 })
